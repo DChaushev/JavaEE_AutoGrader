037efa33d46319f6e00216ad68975c161ab92d0f
Dimitar Chaushev <d.chaushev073@gmail.com>
2015-07-01 11:54:04 +0300

    view/hide source

--- src/main/webapp/scripts/task.js
+++ src/main/webapp/scripts/task.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
         else
         {
             $.post('AutoGrader/task/submit', {user: Cookies.get('AutoGraderUser'), task_id: parseInt(getUrlVar("id")), src_code: $(".form-control").val()}, function (data) {
+
                 if (data.hasOwnProperty("err"))
                     alert(data.err);
                 else
@@ -25,19 +26,38 @@ $(document).ready(function () {
         }
     });
 
+
+
     $.get("AutoGrader/task/" + getUrlVar("id"), function (data) {
 
         $(".entry-title").text(data.task_name);
-        $("#task_file").html("<div> <a href='AutoGrader/task/pdf/" + getUrlVar("id") + "'>" + data.task_name + "</a></div>");
+        $("#task_file").html("<div> <a href='http://" + data.task_file + "'>" + data.task_name + "</a></div>");
+
+        var i = 0;
 
         $.each(data.results, function (index, value) {
+
+
+
+            $("#srcs").append("<div id='src" + i + "' style='display: none;'>" + value.source + "</div>");
+
             $("#task-results").append("<tr>\n\
             <td>n/a</td>\n\
             <td>" + value.status + "</td>\n\
             <td>" + value.score + "</td>\n\
             <td>n/a</td>\n\
-            <td>view source</td>\n\
+            <td><a href='#' class='showsrc' id='src" + i + "' > view source </a></td>\n\
             </tr>");
+
+            i = i + 1;
         });
+
+        $(".showsrc").click(function () {
+            //$('#' + $(this).data('id')).toggle();
+            $("#srcs").children().not('#' + $(this).attr('id')).hide();
+
+            $('#' + $(this).attr('id')).toggle();
+        });
+
     });
 });
